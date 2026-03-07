15a9bab827b2cb1440dcc8be2e9a39463f7bcb43
Michael Leddy <31906313+dev-leddy@users.noreply.github.com>
2026-03-07 09:25:29 -0500

    Set base path for production build in vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ command }) => ({
   plugins: [react()],
+
   base: command === 'serve' ? '/' : '/1490doom-builder/',
+
   server: {
     host: true,
     port: 5173,
