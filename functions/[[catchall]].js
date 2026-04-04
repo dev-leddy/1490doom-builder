2e059a75d5db97aeccb4efa65e45675bfd506f38
Michael Leddy <iamleddy@gmail.com>
2026-04-03 20:06:39 -0400

    fix: load shared company from URL hash on page load

--- functions/[[catchall]].js
+++
@@ -1,9 +0,0 @@
-export function onRequest(context) {
-  const url = new URL(context.request.url)
-  
-  if (url.pathname === '/quiz') {
-    return Response.redirect(url.origin + '/quiz.html', 302)
-  }
-  
-  return context.next()
-}
--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
 import { useRegisterSW } from 'virtual:pwa-register/react'
+import { useEffect } from 'react'
 import { useTrackerStore } from './store/trackerStore'
 import BuilderPage from './builder/BuilderPage'
 import TrackerPage from './tracker/TrackerPage'
 import Toast from './shared/Toast'
 import BetaBanner from './shared/BetaBanner'
-import { useBuilderStore } from './store/builderStore'
+import { useBuilderStore, decodeCompany } from './store/builderStore'
 import RestorePromptModal from './shared/RestorePromptModal'
 
 export default function App() {
   const trackerActive = useTrackerStore(s => s.active)
   const showRestorePrompt = useTrackerStore(s => s.showRestorePrompt)
   const toast = useBuilderStore(s => s.toast)
+  const { doImport } = useBuilderStore()
 
   const { needRefresh: [needRefresh], updateServiceWorker } = useRegisterSW()
+
+  useEffect(() => {
+    const hash = window.location.hash.slice(1)
+    if (hash) {
+      const decoded = decodeCompany(hash)
+      if (decoded) {
+        doImport(hash)
+      }
+    }
+  }, [])
 
   return (
     <div className="app">
